211a6ae5f2178fc160401edf8e1e79eb4570aac3
Stephanie <me@stephaniemariewatson.com>
2023-05-04 10:36:24 -0400

    Basic controller unit test

---
+++ RoomBookingApp.API.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RoomBookingApp.API.Controllers;
+using Shouldly;
+
+namespace RoomBookingApp.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Should_Return_Forecast_Results()
+        {
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            var result = controller.Get();
+
+            
+            result.Count().ShouldBeGreaterThan(1);
+            result.ShouldNotBeNull();
+
+        }
+
+    }
+}
